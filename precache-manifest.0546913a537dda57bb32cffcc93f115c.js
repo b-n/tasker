3454e2a1b934f94141eaec87c0b6afe123c25fa4
Automaton <nayben@gmail.com>
2019-02-13 19:49:53 +0000

    Deploy to GitHub pages [ci skip]

---
+++ precache-manifest.0546913a537dda57bb32cffcc93f115c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c306b2641babf55963a2",
+    "url": "/tasker/static/css/main.71bc759d.chunk.css"
+  },
+  {
+    "revision": "c306b2641babf55963a2",
+    "url": "/tasker/static/js/main.c306b264.chunk.js"
+  },
+  {
+    "revision": "e87847853c35ad4aef30",
+    "url": "/tasker/static/js/1.e8784785.chunk.js"
+  },
+  {
+    "revision": "902719eeabb12797edba",
+    "url": "/tasker/static/js/runtime~main.902719ee.js"
+  },
+  {
+    "revision": "ba32097d125d0c12d10e6a506ba42fba",
+    "url": "/tasker/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ae262495626a78f00d2a7b8a15389921.js"
+  "/tasker/precache-manifest.0546913a537dda57bb32cffcc93f115c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/tasker/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
